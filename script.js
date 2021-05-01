9d5f037bf1126c37ce9aed324c6b103a46177a2c
TaeMin-98 <75558562+TaeMin-98@users.noreply.github.com>
2021-05-01 10:56:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
        $.getJSON('http://api.openweathermap.org/data/2.5/forecast?id=1835848&appid=31ec7260bd9cc0d8d1bf3b02223d7981&units=metric', function(data) {
-           const $icon = data.list[1].weather[0].icon;
-           const $weather = data.list[0].weather[0].main;
-           const $cTemp = data.list[0].main.temp;
-           const $maxTemp = data.list[0].main.temp_max;
-           const $minTemp = data.list[0].main.temp_min;
+         const $weather = data.list[0].weather[0].main;
+         const $cTemp = data.list[0].main.temp;
+         const $maxTemp = data.list[0].main.temp_max;
+         const $minTemp = data.list[0].main.temp_min;
 
-           $('.icon').append($icon);
-           $('.weather').append($weather);
-           $('.ctemp').append($cTemp);
-           $('.hightemp').append($maxTemp);
-           $('.lowtemp').append($minTemp);
-         });
+         $('.weather').append($weather);
+         $('.ctemp').append($cTemp);
+         $('.hightemp').append($maxTemp);
+         $('.lowtemp').append($minTemp);
+       });
