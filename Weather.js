c295dc6971c77308d4256f6af553ad454cb4f2ea
TaeMin-98 <75558562+TaeMin-98@users.noreply.github.com>
2021-05-02 09:32:57 +0900

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -1,38 +1,41 @@
-       $.getJSON('http://api.openweathermap.org/data/2.5/forecast?id=1835848&appid=31ec7260bd9cc0d8d1bf3b02223d7981&units=metric', function(data) {
-         const weather = data.list[0].weather[0].main;
-         const cTemp = data.list[0].main.temp;
-         const maxTemp = data.list[0].main.temp_max;
-         const minTemp = data.list[0].main.temp_min;
+       $.getJSON('http://api.openweathermap.org/data/2.5/weather?id=1835848&appid=31ec7260bd9cc0d8d1bf3b02223d7981&units=metric', function(data) {
+         const weather = data.weather[0].main;
+         const cTemp = data.main.temp;
+         const maxTemp = data.main.temp_max;
+         const minTemp = data.main.temp_min;
+         const cityName = data.name;
+         
+         $('#City').append(cityName);
+         $('#City2').append(cityName);
+         $('.ctemp').prepend(Math.round(cTemp));
+         $('.hightemp').prepend(Math.round(maxTemp));
+         $('.lowtemp').prepend(Math.round(minTemp));
 
-         $('.weather').append(weather);
-         $('.ctemp').prepend(Math.floor(cTemp));
-         $('.hightemp').prepend(Math.floor(maxTemp));
-         $('.lowtemp').prepend(Math.floor(minTemp));
+         
 
          switch (weather) {
            case 'Rain':
-             document.querySelector('.icon').innerHTML = '<img src="img/Rain.png" width = 125px height = 125px/>';
+             document.querySelector('.icon').innerHTML = '<img src="img/Rain.png" width = 120px height = 120px/>';
              break;
 
            case 'Clear':
-             document.querySelector('.icon').innerHTML = '<img src="img/Clear.png" width = 125px height = 125px/>';
+             document.querySelector('.icon').innerHTML = '<img src="img/Clear.png" width = 120px height = 120px/>';
              break;
 
            case 'Clouds':
-             document.querySelector('.icon').innerHTML = '<img src="img/Clouds.png" width = 125px height = 125px/>';
+             document.querySelector('.icon').innerHTML = '<img src="img/Clouds.png" width = 120px height = 120px/>';
              break;
 
            case 'Snow':
-             document.querySelector('.icon').innerHTML = '<img src="img/Snow.png" width = 125px height = 125px/>';
+             document.querySelector('.icon').innerHTML = '<img src="img/Snow.png" width = 120px height = 120px/>';
              break;
 
            case 'Thunderstorm':
-             document.querySelector('.icon').innerHTML = '<img src="img/Thunderstorm.png" width = 125px height = 125px/>';
+             document.querySelector('.icon').innerHTML = '<img src="img/Thunderstorm.png" width = 120px height = 120px/>';
              break;
 
            case 'Mist', 'Smoke', 'Haze', 'Dust', 'Fog', 'Sand', 'Ash', 'Squall':
-             document.querySelector('.icon').innerHTML = '<img src="img/Clear.png" width = 125px height = 125px/>';
+             document.querySelector('.icon').innerHTML = '<img src="img/Clear.png" width = 120px height = 120px/>';
              break;
-
          }
        });
